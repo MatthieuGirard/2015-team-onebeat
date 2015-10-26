352742102e18bed5f19ee3482e545f2414a5ee21
RAKSHA <RAKSHA.MUTH@GMAIL.COM>
2015-10-26 15:22:13 +0100

    Server now has single test JSON object

--- page/views.py
+++ page/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
 def post_list(request):
     return render(request, 'page/post_list.html', {})
+
+def ret_value(request):
+	return JsonResponse({'foo':'bar'})

eb089e27ba3df96f1a6827cb6144481dee65f37e
Drnoodle <hugo.bonnome@gmail.com>
2015-11-07 19:04:03 +0100

    controler for the main activity

--- app/src/main/java/ch/epfl/sweng/team_onebeat/FrontEnd/Controler/ControlerMainActivity.java
+++ app/src/main/java/ch/epfl/sweng/team_onebeat/FrontEnd/Controler/ControlerMainActivity.java
@@ -1,10 +1,15 @@
 package ch.epfl.sweng.team_onebeat.FrontEnd.Controler;
 
+import android.content.Intent;
+import android.util.Log;
+
+import java.net.CookieHandler;
 import java.util.Observable;
 import java.util.Observer;
 
 import ch.epfl.sweng.team_onebeat.Exceptions.DisplayNotImplementedException;
 import ch.epfl.sweng.team_onebeat.FrontEnd.Activity.MainActivity;
+import ch.epfl.sweng.team_onebeat.FrontEnd.Activity.RoomActivity;
 
 /*
 observe machine state and when a state is modified, execute related code, ex :
@@ -29,27 +34,70 @@ public class ControlerMainActivity implements Observer {
     public void update(Observable observable, Object data) {
 
 
-        if(data instanceof MachineState){
+        if(observable instanceof MachineState){
+            Log.d("mainAct", "controler is updated because a machineState has changed his state");
 
-            MachineState ms = (MachineState) data;
+            MachineState ms = (MachineState) observable;
 
             if(ms.getType() == StaticMachine.Type.CONNECTION){
 
                 try {
-                switch(  StaticMachine.ConnectionState.values()[ ms.getState().value() ]  ){
-                    case OFF :
+                    switch(  StaticMachine.ConnectionState.values()[ ms.getState().value() ]  ){
+                        case OFF :
                             activity.display(StaticMachine.ConnectionState.OFF);
-                        break;
-                    case TRY_CONNECT:
-                        activity.display(StaticMachine.ConnectionState.TRY_CONNECT);
-                        break;
-                    case CONNECTED:
-                        break;
+                            break;
+                        case TRY_CONNECT:
 
-                }
+                            Log.d("mainAct", "controler see the machine state in : try_connect state : " +
+                                    "apply the try_connect code to control view");
+
+                            // put display in pending
+                            activity.display(StaticMachine.ConnectionState.TRY_CONNECT);
+
+                            // TODO : create request with the activity text field
+                            // TODO : get a pendingData<SpotifyUser>
+                            // TODO : make a thread :
+                            // run until data ready
+                            // put Machine state : connection to CONNECTED
+
+                            // TODO : delete (wait 2s)
+                            Thread t = new Thread(
+                                    new Runnable(){
+
+                                        @Override
+                                        public void run() {
+
+                                            try{
+                                                Thread.sleep(1000);
+                                                //put machine state to connected
+                                                StaticMachine.get(StaticMachine.Type.CONNECTION)
+                                                        .setState(State.Factory.provide(StaticMachine.ConnectionState.CONNECTED));
+
+                                            }catch(InterruptedException e){}
+
+
+                                        }
+                                    }
+                            );
+
+                            t.start();
+
+
+                            //-----------------------
+                            break;
+                        case CONNECTED:
+                            Log.d("mainAct", "controler see the machine state in : connected state : " +
+                                    "apply the connected code to control view");
+
+                           Intent intent2 = new Intent(activity, RoomActivity.class);
+                           activity.startActivity(intent2);
+                            break;
+
+                    }
                 } catch (DisplayNotImplementedException e) {
                     e.printStackTrace();
                 }
+
 
 
             }

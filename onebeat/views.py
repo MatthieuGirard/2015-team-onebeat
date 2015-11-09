b17365e168dc594b65d1f128bdf2e2605ad46b6a
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-11-09 19:21:52 +0100

    maybe that's gonna work

--- onebeat/views.py
+++ onebeat/views.py
@@ -5,17 +5,17 @@ from .models import User
 
 # Create your views here.
 def existUser(request):
-	userId=request.POST.get('userId',None)
+	userId=request.GET['userId']
 	if (User.objects.filter(userId=userId).exists()):
 		return JsonResponse({'bool':'true'})
 	else:
 		return JsonResponse({'bool':'false'})
 
 def addUser(request):
-	#received_json_data=json.loads(request.POST[])
-	#userId=received_json_data['userId']
-	#ame=received_json_data['name']
-	userId=request.POST.get('userId',None)
-	name=request.POST.get('name',None)
+	received_json_data=json.loads(request.POST['request'])
+	userId=received_json_data['userId']
+	ame=received_json_data['name']
+	#userId=request.POST.get('userId',None)
+	#name=request.POST.get('name',None)
 	User.objects.create(userId=userId,name=name)
 	return JsonResponse({'added':'true', 'user':name})

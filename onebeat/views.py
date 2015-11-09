2754ba2ddee6ae362bc16a8a3e59f402d7ece66d
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-11-09 15:01:57 +0100

    first attempt at treating jsonrequest

--- onebeat/views.py
+++ onebeat/views.py
@@ -3,9 +3,17 @@ from django.http import JsonResponse
 from .models import User
 
 # Create your views here.
+def default(request):
+
+
 def existUser(request):
 	return JsonResponse({'bool':'true'})
 
 def addUser(request):
-	User.objects.create(userId=12345,name='test')
-	return JsonResponse({'added':'true', 'user':'test'})
+	received_json_data=json.loads(request.body)
+	userId=received_json_data['userId']
+	name=received_json_data['name']
+	User.objects.create(userId=userId,name=name)
+	return JsonResponse({'added':'true', 'user':name})
+
+def getUser(request):

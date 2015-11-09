f882a94ba1315f689efb1679342fcd0217b9479d
RAKSHA <RAKSHA.MUTH@GMAIL.COM>
2015-11-09 19:44:35 +0100

    getUser test2

--- onebeat/urls.py
+++ onebeat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # url(r'^$', views.post_list, name='post_list'),
     url(r'^existUser', views.existUser, name='existUser'),
     url(r'^addUser', views.addUser, name='addUser'),
+    url(r'^getUser', views.getUser, name='getUser'),
 ]

e69336c16df74f3b31ab46e7fbea7c45515754cd
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-11-09 19:33:40 +0100

    existUser test

--- onebeat/views.py
+++ onebeat/views.py
@@ -5,7 +5,8 @@ from .models import User
 
 # Create your views here.
 def existUser(request):
-	userId=request.GET['userId']
+	received_json_data=json.loads(request.GET['request'])
+	userId=received_json_data['userId']
 	if (User.objects.filter(userId=userId).exists()):
 		return JsonResponse({'bool':'true'})
 	else:
@@ -15,7 +16,5 @@ def addUser(request):
 	received_json_data=json.loads(request.POST['request'])
 	userId=received_json_data['userId']
 	name=received_json_data['name']
-	#userId=request.POST.get('userId',None)
-	#name=request.POST.get('name',None)
 	User.objects.create(userId=userId,name=name)
 	return JsonResponse({'added':'true', 'user':name})

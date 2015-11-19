06ea73d4a9d6b37a5ae0bd05ea7853713e5a875f
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-11-19 19:16:12 +0100

    Add admin support for new tables.

--- server/onebeat/admin.py
+++ server/onebeat/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import User
+from .models import Room
+from .models import Song
 
 admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Song)

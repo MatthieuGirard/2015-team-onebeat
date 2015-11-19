9be79ce91e1ef7e2f16ad6bc9967759b56893ab6
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-11-19 16:05:29 +0100

    Add backend server.

---
+++ server/onebeat/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+	userId = models.IntegerField()
+	userId.primary_key = True
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name

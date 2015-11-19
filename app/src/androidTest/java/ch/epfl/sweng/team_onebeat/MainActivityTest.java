7b62496ba88a6191fd6ea3d160c63bf96cb3d132
Valérian Rousset <valerian.rousset@epfl.ch>
2015-11-19 10:38:36 +0100

    Remove useless test and add example.

---
+++ app/src/androidTest/java/ch/epfl/sweng/team_onebeat/MainActivityTest.java
@@ -0,0 +1,34 @@
+package ch.epfl.sweng.team_onebeat;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.team_onebeat.FrontEnd.Activity.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void launchActivity() {
+
+    }
+
+    @Test
+    public void clickConnection() {
+        onView(withId(R.id.button)).perform(click());
+    }
+}

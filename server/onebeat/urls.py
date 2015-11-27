6b519f49695da532a17c27473def45730421dc6a
RAKSHA <RAKSHA.MUTH@GMAIL.COM>
2015-11-27 17:51:18 +0100

    fixed urls.py file

--- server/onebeat/urls.py
+++ server/onebeat/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^addUser', views.addUser, name='addUser'),
     url(r'^getSong', views.getSong, name='getSong'),
     url(r'^addSong', views.addSong, name='addSong'),
-    url(r'^getRoom', views.addSong, name='getRoom'),
-    url(r'^createRoom', views.addSong, name='addRoom'),
+    url(r'^getRoom', views.getRoom, name='getRoom'),
+    url(r'^createRoom', views.createRoom, name='createRoom'),
+    url(r'^joinRoom', views.joinRoom, name='joinRoom'),
 ]

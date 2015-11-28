35add08f7eda4d574d41bf77bebc93e08d0f62fb
RAKSHA <RAKSHA.MUTH@GMAIL.COM>
2015-11-28 12:34:22 +0100

    still tweaking urls.py

--- server/onebeat/urls.py
+++ server/onebeat/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^getUser', views.getUser, name='getUser'),
-    url(r'^addUser', views.addUser, name='addUser'),
-    url(r'^getSong', views.getSong, name='getSong'),
-    url(r'^addSong', views.addSong, name='addSong'),
-    url(r'^getRoom', views.getRoom, name='getRoom'),
-    url(r'^createRoom', views.createRoom, name='createRoom'),
-    url(r'^joinRoom', views.joinRoom, name='joinRoom'),
+    url(r'^onebeat/', include('onebeat.urls')),
+    url(r'^getUser/', views.getUser, name='getUser'),
+    url(r'^addUser/', views.addUser, name='addUser'),
+    url(r'^getSong/', views.getSong, name='getSong'),
+    url(r'^addSong/', views.addSong, name='addSong'),
+    url(r'^getRoom/', views.getRoom, name='getRoom'),
+    url(r'^createRoom/', views.createRoom, name='createRoom'),
+    url(r'^joinRoom/', views.joinRoom, name='joinRoom'),
 ]

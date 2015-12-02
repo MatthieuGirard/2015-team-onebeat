41e903a8d7bd7cb50f2755a8ebd025d85a01da7d
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-12-02 13:35:47 +0100

    song duration is now a float instead a string

--- server/onebeat/models.py
+++ server/onebeat/models.py
@@ -11,7 +11,7 @@ class User(models.Model):
 class Song(models.Model):
 	artist = models.CharField(max_length=100)
 	title = models.CharField(max_length=200)
-	duration = models.CharField(max_length=20)
+	duration = models.FloatField()
 	spotifyRef = models.CharField(max_length=100)
 
 	def __str__(self):

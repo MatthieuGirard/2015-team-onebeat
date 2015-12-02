f1c973a55ab103ebf40149d8ad668cfc666a1bd9
Pierre-Antoine Desplaces <pierre-antoine.desplaces@epfl.ch>
2015-12-02 13:33:22 +0100

    user ids are now string, according to spotify's gestion of ids

--- server/onebeat/models.py
+++ server/onebeat/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-	userId = models.IntegerField()
+	userId = models.CharField(max_length=200)
 	userId.primary_key = True
 	name = models.CharField(max_length=100)
 
